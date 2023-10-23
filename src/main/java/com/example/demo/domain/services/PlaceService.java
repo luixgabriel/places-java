f0266d05abe1bfad3f6767dcae7dedf1b0705ced
Luis Gabriel <luisgasbriel@gmail.com>
2023-10-23 11:12:11 -0300

    feat: add slugfy config

--- src/main/java/com/example/demo/domain/services/PlaceService.java
+++ src/main/java/com/example/demo/domain/services/PlaceService.java
@@ -1,23 +1,32 @@
 package com.example.demo.domain.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.application.dtos.PlaceRequest;
 import com.example.demo.domain.models.Place;
 import com.example.demo.domain.useCases.repositories.IPlaceRepository;
+import com.github.slugify.Slugify;
 
+import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@EnableR2dbcAuditing
 @Service
 public class PlaceService {
     
     @Autowired
     IPlaceRepository placeRepository;
+    private Slugify slg;
+
+    public PlaceService(){
+        this.slg = slg.builder().build();
+    }
 
     public Mono<Place> create(PlaceRequest data){
-        var place = new Place(null, data.name(), data.slug(), data.state(), data.createdAt(), data.updatedAt());
+        var place = new Place(null, data.name(), slg.slugify(data.name()), data.city(), data.state(), data.createdAt(), data.updatedAt());
         return placeRepository.save(place);
     }
 
